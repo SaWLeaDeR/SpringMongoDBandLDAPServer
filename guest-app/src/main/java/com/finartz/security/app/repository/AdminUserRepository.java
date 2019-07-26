e8d58ac02eef5497e89723890f4d337add29a01b
SaWLeaDeR <U140709015>
2019-07-26 11:28:47 +0300

    fixup! intellij

--- guest-app/src/main/java/com/finartz/security/app/repository/AdminUserRepository.java
+++ guest-app/src/main/java/com/finartz/security/app/repository/AdminUserRepository.java
@@ -2,6 +2,7 @@ package com.finartz.security.app.repository;
 
 
 import com.finartz.security.app.domain.AdminUser;
+import com.finartz.security.app.domain.Guest;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
